feaaf318e9aad2b6e730797cf75db0e2b681828a
yangdi <41617440+Amsterdamyd@users.noreply.github.com>
2020-09-16 23:27:45 -0700

    add another new function

--- src/com/yangdi/leetcode/linkedlist/IntersectionNode.java
+++ src/com/yangdi/leetcode/linkedlist/IntersectionNode.java
@@ -33,4 +33,50 @@ public class IntersectionNode {
 
         return null;
     }
+
+    public SinglyListNode getIntersectionNode2(SinglyListNode headA, SinglyListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+
+        int lengthA = 0;
+        int lengthB = 0;
+        SinglyListNode tempHeadA = headA;
+        SinglyListNode tempHeadB = headB;
+
+        //Calculate the length of listA.
+        while (tempHeadA != null) {
+            lengthA++;
+            tempHeadA = tempHeadA.next;
+        }
+
+        //Calculate the length of listB.
+        while (tempHeadB != null) {
+            lengthB++;
+            tempHeadB = tempHeadB.next;
+        }
+
+        //Move the pointer of longer list first.
+        int length = lengthA > lengthB ? (lengthA - lengthB) : (lengthB - lengthA);
+        for (int i = 0; i < length; i++) {
+            if (lengthA > lengthB && headA != null) {
+                headA = headA.next;
+            } else if (lengthA <= lengthB && headB != null) {
+                headB = headB.next;
+            } else {
+            }
+        }
+
+        //Then move two pointers of lists respectively at the same time.
+        while (headA != null && headB != null) {
+            if (headA == headB) {
+                return headA;
+            } else {
+                headA = headA.next;
+                headB = headB.next;
+            }
+        }
+
+        return null;
+    }
 }

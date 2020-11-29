e279ae99b80dffa65eb69ced2f06d305d45b0702
yangdi <41617440+Amsterdamyd@users.noreply.github.com>
2020-11-28 23:57:17 -0800

    max depth and pow

---
+++ src/com/yangdi/leetcode/recursion/MaximumDepth.java
@@ -0,0 +1,104 @@
+package com.yangdi.leetcode.recursion;
+
+import com.yangdi.leetcode.binarytree.TreeNode;
+
+import java.util.LinkedList;
+
+public class MaximumDepth {
+
+    //recursion (no global variable)
+    public int maxDepth(TreeNode root) {
+        int maxDepth = 0;
+        if (root == null) {
+            return maxDepth;
+        }
+
+        return helper(root, 1, maxDepth);
+    }
+
+    int helper(TreeNode root, int depth, int maxDepth) {
+        if (root.left == null && root.right == null) {
+            maxDepth = Math.max(depth, maxDepth);
+            return maxDepth;
+        }
+
+        if (root.left != null) {
+            maxDepth = helper(root.left, depth + 1, maxDepth);
+        }
+
+        if (root.right != null) {
+            maxDepth = helper(root.right, depth + 1, maxDepth);
+        }
+
+        return maxDepth;
+    }
+
+    //recursion (use global variable)
+    private int MaxDepth = 0;
+    public int maxDepth2(TreeNode root) {
+        int depth = 0;
+        if (root == null) {
+            return depth;
+        }
+
+        findMaxDepth(root, 1);
+        return MaxDepth;
+    }
+
+    void findMaxDepth(TreeNode node, int depth) {
+        if (node.left == null && node.right == null) {
+            MaxDepth = Math.max(depth, MaxDepth);
+            return;
+        }
+
+        if (node.left != null) {
+            findMaxDepth(node.left, depth + 1);
+        }
+
+        if (node.right != null) {
+            findMaxDepth(node.right, depth + 1);
+        }
+    }
+
+    //recursion (postorder)
+    public int maxDepth3(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int left_height = maxDepth(root.left);
+            int right_height = maxDepth(root.right);
+
+            return Math.max(left_height, right_height) + 1;
+        }
+    }
+
+    //iteration
+    public int maxDepth4(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        LinkedList<Integer> depths = new LinkedList<>();
+
+        stack.add(root);
+        depths.add(1);
+
+        int depth = 0, currentDepth = 0;
+
+        while (!stack.isEmpty()) {
+            root = stack.pollLast();
+            currentDepth = depths.pollLast();
+
+            if (root != null) {
+                depth = Math.max(depth, currentDepth);
+                stack.add(root.left);
+                stack.add(root.right);
+                depths.add(currentDepth + 1);
+                depths.add(currentDepth + 1);
+            }
+        }
+
+        return depth;
+    }
+}
---
+++ src/com/yangdi/leetcode/recursion/Pow.java
@@ -0,0 +1,119 @@
+package com.yangdi.leetcode.recursion;
+
+public class Pow {
+
+    public double myPow(double x, int n) {
+        if (x == 0) {
+            return Double.valueOf(0);
+        }
+
+        if (n >= 0) {
+            return helper(x, n);
+        } else {
+            // When n is Integer.MIN_VALUE, (-n) is still negative, and equal to itself.
+            return 1/helper(x, -n);
+        }
+    }
+
+    /**
+     * Recursion solution (by myself)
+     * Time Complexity: O(logN)
+     * Space Complexity: O(logN)
+     */
+    double helper(double x, int n) {
+        if (n == 0) {
+            return Double.valueOf(1);
+        }
+        if (n == 1) {
+            return x;
+        }
+
+        if (n % 2 == 0) {
+            return helper(x * x, n / 2);
+        } else {
+            return x * helper(x * x, n / 2);
+        }
+    }
+
+    /**
+     * Iteration solution (by myself)
+     * if n is Integer.MIN_VALUE,
+     * Time Complexity: O(logN)
+     * Space Complexity: O(1)
+     */
+    static double helper2(double x, int n) {
+        double sum = 1;
+
+        while (n >= 1 || n <= -1) {
+            if (n % 2 == 0) {
+                x = x * x;
+                n = n / 2;
+            } else {
+                sum *= x;
+                x = x * x;
+                n = n / 2;
+            }
+        }
+
+        return sum;
+    }
+
+    /**
+     * from leetcode
+     */
+    public double myPow2(double x, int n) {
+        long N = n; // transfer int into long to avoid -Integer.MIN_VALUE issue
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        return fastPow(x, N);
+    }
+    private double fastPow(double x, long n) {
+        if (n == 0) {
+            return 1.0;
+        }
+
+        double half = fastPow(x, n / 2);
+
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+
+    /**
+     * from leetcode
+     */
+    public double myPow3(double x, int n) {
+        long N = n; // transfer int into long to avoid -Integer.MIN_VALUE issue
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        double ans = 1;
+        double current_product = x;
+
+        for (long i = N; i > 0; i /= 2) {
+            if ((i % 2) == 1) {
+                ans = ans * current_product;
+            }
+            current_product = current_product * current_product;
+        }
+
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(Integer.MIN_VALUE * -1);
+        System.out.println(Long.MIN_VALUE * -1);
+        System.out.println(Double.MIN_VALUE * -1);
+        System.out.println(Short.MIN_VALUE * -1);
+        System.out.println(Byte.MIN_VALUE * -1);
+        System.out.println(1 / helper2(2, -2147483648));
+    }
+}
